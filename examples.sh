54e28aaee8e72e30bb58dd7a79b7380a35d0e41e
CHLoschen <chrissly31415@gmail.com>
2020-11-25 20:22:40 +0100

    minor changes and updates

--- examples.sh
+++ examples.sh
@@ -1,58 +1,58 @@
 #!/bin/bash
 
 #basic training & evaluation with gradient boosting
-#./ml_bond_parser.py --train all_sdf/bradley_mp_dataset_part1_UFF.sdf
-#./ml_bond_parser.py --eval all_sdf/bradley_mp_dataset_part2_UFF.sdf
-#./ml_bond_parser.py --train all_sdf/bradley_mp_dataset_part1_ETKDG.sdf
-#./ml_bond_parser.py --eval all_sdf/bradley_mp_dataset_part2_ETKDG.sdf
-#./ml_bond_parser.py --train all_sdf/bradley_mp_dataset_part1_MMFF.sdf
-#./ml_bond_parser.py --eval all_sdf/bradley_mp_dataset_part2_MMFF.sdf
-#./ml_bond_parser.py --train 3dqsar_sd/3dqsar_all_train.sdf
-#./ml_bond_parser.py --eval 3dqsar_sd/3dqsar_all_test.sdf
-#./ml_bond_parser.py --train gdb1/gdb1_train.sdf 
-#./ml_bond_parser.py --eval gdb1/gdb1_test.sdf 
-#./ml_bond_parser.py --train --noH pdbbind_refined/sdf/pdbbind_refined_clean_noh_train.sdf
-#./ml_bond_parser.py --eval --noH pdbbind_refined/sdf/pdbbind_refined_clean_noh_test.sdf
+#./mamba.py --train bradley_mp_dataset_part1_UFF.sdf
+#./mamba.py --eval bradley_mp_dataset_part2_UFF.sdf
+#./mamba.py --train bradley_mp_dataset_part1_ETKDG.sdf
+#./mamba.py --eval bradley_mp_dataset_part2_ETKDG.sdf
+#./mamba.py --train bradley_mp_dataset_part1_MMFF.sdf
+#./mamba.py --eval bradley_mp_dataset_part2_MMFF.sdf
+#./mamba.py --train 3dqsar_sd/3dqsar_all_train.sdf
+#./mamba.py --eval 3dqsar_sd/3dqsar_all_test.sdf
+#./mamba.py --train gdb1/gdb1_train.sdf 
+#./mamba.py --eval gdb1/gdb1_test.sdf 
+#./mamba.py --train --noH pdbbind_refined/sdf/pdbbind_refined_clean_noh_train.sdf
+#./mamba.py --eval --noH pdbbind_refined/sdf/pdbbind_refined_clean_noh_test.sdf
 
 #train on subsample of all datasets
-#./ml_bond_parser.py --train --sample 0.3 all_sdf/train/all_train.sdf
-#./ml_bond_parser.py --eval all_sdf/bradley_mp_dataset_part2_UFF.sdf
-#./ml_bond_parser.py --eval all_sdf/bradley_mp_dataset_part2_ETKDG.sdf
-#./ml_bond_parser.py --eval all_sdf/bradley_mp_dataset_part2_MMFF.sdf
-#./ml_bond_parser.py --eval 3dqsar_sd/3dqsar_all_test.sdf
-#./ml_bond_parser.py --eval gdb1/gdb1_test.sdf 
+#./mamba.py --train --sample 0.3 all_train.sdf
+#./mamba.py --eval bradley_mp_dataset_part2_UFF.sdf
+#./mamba.py --eval bradley_mp_dataset_part2_ETKDG.sdf
+#./mamba.py --eval bradley_mp_dataset_part2_MMFF.sdf
+#./mamba.py --eval 3dqsar_sd/3dqsar_all_test.sdf
+#./mamba.py --eval gdb1/gdb1_test.sdf 
 
 #evaluate openbabel
-#./ml_bond_parser.py --evalOB --noH all_sdf/bradley_mp_dataset_part2_UFF.sdf
-#./ml_bond_parser.py --evalOB --noH all_sdf/bradley_mp_dataset_part2_MMFF.sdf
-#./ml_bond_parser.py --evalOB --noH all_sdf/bradley_mp_dataset_part2_ETKDG.sdf
-#./ml_bond_parser.py --evalOB --noH all_sdf/3dqsar_all.sdf
-#./ml_bond_parser.py --evalOB --noH pdbbind_refined/sdf/pdbbind_refined_clean_noh_test.sdf
-#./ml_bond_parser.py --evalOB --noH gdb1/gdb1_test.sdf 
+#./mamba.py --evalOB --noH bradley_mp_dataset_part2_UFF.sdf
+#./mamba.py --evalOB --noH bradley_mp_dataset_part2_MMFF.sdf
+#./mamba.py --evalOB --noH bradley_mp_dataset_part2_ETKDG.sdf
+#./mamba.py --evalOB --noH 3dqsar_all.sdf
+#./mamba.py --evalOB --noH pdbbind_refined/sdf/pdbbind_refined_clean_noh_test.sdf
+#./mamba.py --evalOB --noH gdb1/gdb1_test.sdf 
 
 #use RF for training
-#./ml_bond_parser.py --train --useRF all_sdf/bradley_mp_dataset_part1_UFF.sdf
-#./ml_bond_parser.py --eval all_sdf/bradley_mp_dataset_part2_UFF.sdf
-#./ml_bond_parser.py --train --useRF all_sdf/bradley_mp_dataset_part1_ETKDG.sdf
-#./ml_bond_parser.py --eval all_sdf/bradley_mp_dataset_part2_ETKDG.sdf
-#./ml_bond_parser.py --train --useRF all_sdf/bradley_mp_dataset_part1_MMFF.sdf
-#./ml_bond_parser.py --eval all_sdf/bradley_mp_dataset_part2_MMFF.sdf
-#./ml_bond_parser.py --train --useRF 3dqsar_sd/3dqsar_all_train.sdf
-#./ml_bond_parser.py --eval 3dqsar_sd/3dqsar_all_test.sdf
-#./ml_bond_parser.py --train --useRF gdb1/gdb1_train.sdf 
-#./ml_bond_parser.py --eval gdb1/gdb1_test.sdf 
-#./ml_bond_parser.py --train --useRF --noH pdbbind_refined/sdf/pdbbind_refined_clean_noh_train.sdf
-#./ml_bond_parser.py --eval --noH pdbbind_refined/sdf/pdbbind_refined_clean_noh_test.sdf
+#./mamba.py --train --useRF bradley_mp_dataset_part1_UFF.sdf
+#./mamba.py --eval bradley_mp_dataset_part2_UFF.sdf
+#./mamba.py --train --useRF bradley_mp_dataset_part1_ETKDG.sdf
+#./mamba.py --eval bradley_mp_dataset_part2_ETKDG.sdf
+#./mamba.py --train --useRF bradley_mp_dataset_part1_MMFF.sdf
+#./mamba.py --eval bradley_mp_dataset_part2_MMFF.sdf
+#./mamba.py --train --useRF 3dqsar_sd/3dqsar_all_train.sdf
+#./mamba.py --eval 3dqsar_sd/3dqsar_all_test.sdf
+#./mamba.py --train --useRF gdb1/gdb1_train.sdf 
+#./mamba.py --eval gdb1/gdb1_test.sdf 
+#./mamba.py --train --useRF --noH pdbbind_refined/sdf/pdbbind_refined_clean_noh_train.sdf
+#./mamba.py --eval --noH pdbbind_refined/sdf/pdbbind_refined_clean_noh_test.sdf
 
-#./ml_bond_parser.py --train --useRF --sample 0.3 all_sdf/train/all_train.sdf
-#./ml_bond_parser.py  --eval --sample 0.3 all_sdf/train/all_train.sdf
-#./ml_bond_parser.py  --analyze dummy
+#./mamba.py --train --useRF --sample 0.3 all_train.sdf
+#./mamba.py  --eval --sample 0.3 all_train.sdf
+#./mamba.py  --analyze dummy
 
-#./ml_bond_parser.py --eval all_sdf/bradley_mp_dataset_part2_UFF.sdf
-#./ml_bond_parser.py --eval all_sdf/bradley_mp_dataset_part2_MMFF.sdf
-#./ml_bond_parser.py --eval all_sdf/bradley_mp_dataset_part2_ETKDG.sdf
-#./ml_bond_parser.py --eval 3dqsar_sd/3dqsar_all_test.sdf
-#./ml_bond_parser.py --eval gdb1/gdb1_test.sdf 
+#./mamba.py --eval bradley_mp_dataset_part2_UFF.sdf
+#./mamba.py --eval bradley_mp_dataset_part2_MMFF.sdf
+#./mamba.py --eval bradley_mp_dataset_part2_ETKDG.sdf
+#./mamba.py --eval 3dqsar_sd/3dqsar_all_test.sdf
+#./mamba.py --eval gdb1/gdb1_test.sdf 
 
-./ml_bond_parser.py --train --iterative --useRF --sample 0.3 all_sdf/train/all_train.sdf
+./mamba.py --train --iterative --useRF --sample 0.3 all_train.sdf
 
